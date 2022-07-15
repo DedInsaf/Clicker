8f3b114ed5184577e00f47ca67bfe378827b7428
Insaf <inurtdinov@outlook.com>
2022-07-15 13:48:52 +0300

    second commit
    Second Commit

---
+++ Clicker/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] int money;
+    public Text moneyText;
+    public int total_money;
+
+    public void ButtonClick()
+    {
+        money++;
+        total_money++;
+        PlayerPrefs.SetInt("money", money);
+        PlayerPrefs.SetInt("total_money", total_money);
+    }
+
+    void Start()
+    {
+        money = PlayerPrefs.GetInt("money");
+        total_money = PlayerPrefs.GetInt("total_money");
+    }
+
+    void Update()
+    {
+        moneyText.text = money.ToString();
+    }
+
+    public void ToAchievements()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
